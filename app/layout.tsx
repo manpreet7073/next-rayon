70ac5d413c63b9cef6a3d9c1eed07869ce6eec92
meet-dev <tom@dimoapp.com>
2025-05-02 16:38:05 +0530

    add-Head

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css"
 import Navbar from "@/components/navbar"
 import Footer from "@/components/footer"
 import FloatingCTA from "@/components/floating-cta"
+import { Head } from "next/document"
 
 // Optimize font loading
 const manrope = Manrope({
@@ -17,7 +18,6 @@ export const metadata: Metadata = {
   title: "Rayon Web Solutions | Your Vision, Our Code",
   description:
     "Turning Ideas into Reality - Web Development, App Development, DevOps & Cloud, UI/UX Design, LMS & Integration, QA & Automation",
-  generator: "v0.dev",
 }
 
 export default function RootLayout({
@@ -27,6 +27,41 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        {/* Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-68457LHBSZ`}
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-68457LHBSZ');
+            `,
+          }}
+        ></script>
+
+        {/* Facebook Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '23863632866572138');
+              fbq('track', 'PageView');
+            `,
+          }}
+        ></script>
+      </Head>
       <body
         className={`${manrope.variable} font-sans bg-gradient-to-b from-gray-950 to-gray-900 text-gray-100 min-h-screen`}
       >
